216f5e0846465cb5fb412a08f367e87aa8ff2e1a
MARTIN HANSEN <marrtt1234@gmail.com>
2022-10-17 01:38:24 +0000

    cleaned up style and code.

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function City(name, hourOpen, openMeridiem, hourClose, closeMeridiem, min, max, 
 }
 
 let seattle = new City('Seattle', 6, 'a.m.', 7, 'p.m.', 23, 65, 6.3);
-let tokyo = new City('Tokyo', 5, 'a.m.', 7, 'p.m.', 3, 24, 1.2);
+let tokyo = new City('Tokyo', 6, 'a.m.', 7, 'p.m.', 3, 24, 1.2);
 let dubai = new City('Dubai', 6, 'a.m.', 7, 'p.m.', 23, 65, 6.3);
 let paris = new City('Paris', 6, 'a.m.', 7, 'p.m.', 11, 38, 3.7);
 let lima = new City('Lima', 6, 'a.m.', 7, 'p.m.', 2, 16, 4.6);
@@ -22,27 +22,6 @@ let lima = new City('Lima', 6, 'a.m.', 7, 'p.m.', 2, 16, 4.6);
 //table title and initialization
 let titleNum = ['1 a.m.', '2 a.m.', '3 a.m.', '4 a.m.', '5 a.m.', '6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.', '12 a.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.', '8 p.m.', '9 p.m.', '10 p.m.', '11 p.m.'];
 // const cookieCharts = document.getElementById('cookieCharts');
-
-function printHeader() {
-  let table = document.querySelector('thead');
-
-  let title = document.createElement('tr');
-  table.appendChild(title);
-
-  let titleBlank = document.createElement('td');
-  titleBlank.textContent = '';
-  title.appendChild(titleBlank);
-
-  for (let i = 0; i < titleNum.length; i++) {
-    let listItem = document.createElement('td');
-    listItem.textContent = `${titleNum[i]}`;
-    title.appendChild(listItem);
-  }
-
-  let titleTotal = document.createElement('td');
-  titleTotal.textContent = 'Total';
-  title.appendChild(titleTotal);
-}
 
 //adds all items in an array together
 function mathArray(idArr, opp) {
@@ -63,38 +42,51 @@ function mathArray(idArr, opp) {
   }
 }
 
+//helper function: makes and appends element.
+function elementMaker(newEl, content, parent){
+  let title = document.createElement(newEl);
+  title.textContent = content;
+  parent.appendChild(title);
+
+}
+//prints header
+function printHeader() {
+  let table = document.querySelector('thead');
+  let title = document.createElement('tr');
+  table.appendChild(title);
+  elementMaker('td', ' ', title);
+
+  for (let i = 0; i < titleNum.length; i++) {
+    elementMaker('td', `${titleNum[i]}`, title);
+  }
+  elementMaker('td', 'Total', title);
+}
+
 //prints footer
 function printFooter() {
   let totalTempArray = [];
+  let totalArray = [];
   let table = document.querySelector('tFoot');
   let tr = document.createElement('tr');
   tr.setAttribute('id', 'tChild');
   let total = 0;
   table.appendChild(tr);
   //creates title in DOM
-  let rowStart = document.createElement('td');
-  rowStart.textContent = 'Total';
-  tr.appendChild(rowStart);
-
+  elementMaker('td', 'Total', tr);
   for (let i = 0; i < titleNum.length; i++) {
     let listClass = document.getElementsByClassName(`${titleNum[i]}`);
     for (let l = 0; l < listClass.length; l++) {
       totalTempArray[l] = parseInt(listClass[l].innerHTML) || 0;
       total = mathArray(totalTempArray, '+');
+      totalArray[i] = total;
     }
     if (Number.isFinite(total)) {
-      let listItem = document.createElement('td');
-      listItem.textContent = total;
-      tr.appendChild(listItem);
+      elementMaker('td', total, tr);
     } else {
-      let listItem = document.createElement('td');
-      listItem.textContent = '';
-      tr.appendChild(listItem);
+      elementMaker('td', '', tr);
     }
   }
-  let listItem = document.createElement('td');
-  listItem.textContent = '';
-  tr.appendChild(listItem);
+  elementMaker('td', mathArray(totalArray, '+'), tr);
 }
 
 //makes an array with the open and close times of given city
@@ -131,10 +123,7 @@ City.prototype.printList = function (city) {
   let tr = document.createElement('tr');
   table.appendChild(tr);
   //creates title in DOM
-  let rowStart = document.createElement('td');
-  rowStart.textContent = this.name;
-  tr.appendChild(rowStart);
-  //var initialization
+  elementMaker('td', this.name, tr);
   let hours = city.makeTimeArray(city);
   let dailyTotal = 0;
   let CPH = [];
@@ -166,6 +155,7 @@ City.prototype.printList = function (city) {
   listItem.textContent = dailyTotal;
   listItem.setAttribute('class', 'dailyTotal');
   tr.appendChild(listItem);
+  //Refresh footer
   let tFoot = document.getElementById('tFoot');
   let tChild = document.getElementById('tChild');
   tFoot.removeChild(tChild);
@@ -185,7 +175,6 @@ function makeCity(event) {
   cityName.printList(cityName);
   event.preventDefault();
 }
-
 
 //call functions
 printHeader();
